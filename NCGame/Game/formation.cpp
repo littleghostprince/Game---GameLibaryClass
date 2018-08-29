c6f97fc58c02358c0263f1b98573cc503174015b
littleghostprince <amejia@student.neumont.edu>
2018-08-29 14:32:33 -0600

    8/29
    formation!

--- NCGame/Game/formation.cpp
+++ NCGame/Game/formation.cpp
@@ -17,16 +17,35 @@ void Formation::Create()
 	m_attackPaths.push_back({ Vector2D(50.0f, 400.0f), Vector2D(500.0f, 500.0f), Vector2D(350.0f, 350.0f), Vector2D(550.0f, 650.0f) });
 
 	m_formation =
-	{ { { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(100.0f, 160.0f), this }, 0.0f, false },
-	{ { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(140.0f, 160.0f), this }, 0.35f,false },
-	{ { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(100.0f, 200.0f), this }, 0.7f, false },
-	{ { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(140.0f, 200.0f), this }, 1.05f, false },
-	{ { Enemy::BOSS, Enemy::RIGHT, 300.0f,400.0, Vector2D(100.0f, 80.0f), this }, 0.0f, false },
-	{ { Enemy::BOSS, Enemy::RIGHT, 300.0f,400.0, Vector2D(140.0f, 80.0f), this }, 0.35f, false },
-	{ { Enemy::BUG, Enemy::RIGHT, 300.0f,100.0, Vector2D(100.0f, 120.0f), this }, 0.7f, false },
-	{ { Enemy::BUG, Enemy::RIGHT, 300.0f,100.0, Vector2D(140.0f, 120.0f), this }, 1.05f, false },
-	{ {Enemy::BUG2, Enemy::LEFT, 300.0f,50.0, Vector2D(100.0f, 240.0f), this }, 1.35f, false },
-	{ { Enemy::BUG2, Enemy::LEFT, 300.0f,50.0, Vector2D(140.0f, 240.0f), this }, 1.70f, false },
+		//sec row
+	{ { { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(400.0f, 100.0f), this }, 0.0f, false },
+	{ { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(440.0f, 100.0f), this }, 0.35f,false },
+	{ { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(480.0f, 100.0f), this }, 0.7f,false },
+	{ { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(360.0f, 100.0f), this }, 1.05f,false },
+	{ { Enemy::BEE, Enemy::LEFT, 300.0f,200.0, Vector2D(320.0f, 100.0f), this }, 1.35f,false },
+
+	{ { Enemy::BEE, Enemy::RIGHT, 300.0f,200.0, Vector2D(280.0f, 100.0f), this }, 2.0f,false },
+	{ { Enemy::BEE, Enemy::RIGHT, 300.0f,200.0, Vector2D(240.0f, 100.0f), this }, 2.05f,false },
+	{ { Enemy::BEE, Enemy::RIGHT, 300.0f,200.0, Vector2D(200.0f, 100.0f), this }, 2.35f,false },
+		//thrid row
+	{ { Enemy::BUG, Enemy::LEFT, 300.0f,200.0, Vector2D(400.0f, 140.0f), this }, 1.70f, false },
+	{ { Enemy::BUG, Enemy::LEFT, 300.0f,200.0, Vector2D(480.0f, 140.0f), this }, 2.05f, false },
+	{ { Enemy::BUG, Enemy::LEFT, 300.0f,200.0, Vector2D(440.0f, 140.0f), this }, 2.35f, false },
+
+		//bosses top row
+	{ { Enemy::BOSS, Enemy::RIGHT, 300.0f,400.0, Vector2D(400.0f, 65.0f), this }, 0.0f, false },
+	{ { Enemy::BOSS, Enemy::RIGHT, 300.0f,400.0, Vector2D(440.0f, 65.0f), this }, 0.35f, false },
+	{ { Enemy::BOSS, Enemy::RIGHT, 300.0f,400.0, Vector2D(360.0f, 65.0f), this }, 0.7f, false },
+	{ { Enemy::BOSS, Enemy::RIGHT, 300.0f,400.0, Vector2D(320.0f, 65.0f), this }, 1.05f, false },
+	{ { Enemy::BOSS, Enemy::RIGHT, 300.0f,400.0, Vector2D(280.0f, 65.0f), this }, 1.35f, false },
+	{ { Enemy::BOSS, Enemy::RIGHT, 300.0f,400.0, Vector2D(240.0f, 65.0f), this }, 1.70f, false },
+		//thrid row
+	{ {Enemy::BUG2, Enemy::LEFT, 300.0f,50.0, Vector2D(360.0f, 140.0f), this }, 2.70f, false },
+	{ { Enemy::BUG2, Enemy::LEFT, 300.0f,50.0, Vector2D(320.0f, 140.0f), this }, 3.0f, false },
+
+	{ { Enemy::BUG2, Enemy::RIGHT, 300.0f,50.0, Vector2D(280.0f, 140.0f), this }, 2.70f, false },
+	{ { Enemy::BUG, Enemy::RIGHT, 300.0f,50.0, Vector2D(240.0f, 140.0f), this }, 3.0f, false },
+	{ { Enemy::BUG, Enemy::RIGHT, 300.0f,50.0, Vector2D(200.0f, 140.0f), this }, 3.05f, false },
 	};
 	m_timer = 0.0f;
 
